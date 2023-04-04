00d31618c8c0f21956b16bdb1123c2a2f8dd915f
eunnho <taemin4u@khu.ac.kr>
2023-04-05 02:55:37 +0900

    add: midi값, pitch 배열 atom 추가

--- atom/atom.ts
+++ atom/atom.ts
@@ -4,3 +4,13 @@ export const iconType = atom({
   key: 'icon',
   default: 'circle',
 });
+
+export const midiValue = atom({
+  key: 'midi',
+  default: 0,
+});
+
+export const pitchArray = atom<number[]>({
+  key: 'pitchArray',
+  default: [],
+});
